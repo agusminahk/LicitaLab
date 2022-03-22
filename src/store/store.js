2b573fd3330534b23055e370800c7fea4b10e178
agus minahk <agusminahk99@gmail.com>
2022-03-22 14:03:50 -0300

    armado de estructura redux

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import logger from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit';
+import { tasksSlice } from './tasksSlice.js';
+
+export const store = configureStore({
+    middleware: (mw) => mw().concat(logger),
+    reducer: {
+        tasks: tasksSlice,
+    },
+});
