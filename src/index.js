d1a7ca26e433456dbaafcef58be978afa7cd3636
agus minahk <agusminahk99@gmail.com>
2022-03-22 11:56:09 -0300

    add custom theme con nuevos nombres para  breakpoints

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import { StyledEngineProvider, createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            ...defaultTheme.breakpoints.values,
+            mobile: 0,
+            tablet: 600,
+            netbook: 900,
+            laptop: 1240,
+            desktop: 1500,
+        },
+    },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </StyledEngineProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
